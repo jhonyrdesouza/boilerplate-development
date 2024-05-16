1fb7a13ecf2700c2554e9ee09be38a01a4755a2a
jhonyrdesouza <jhony.souza@tegrus.com.br>
2024-05-16 17:35:43 -0300

    feat: add middleware for Next.js internationalization

---
+++ apps/client/src/middleware.ts
@@ -0,0 +1,6 @@
+import createMiddleware from "next-intl/middleware";
+import { defaultLocale, localePrefix, locales } from "./plugins/i18n/config";
+
+export default createMiddleware({ locales, defaultLocale, localePrefix });
+
+export const config = { matcher: ["/", "/(br|en)/:path*"] };
