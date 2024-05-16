8160cc2d4ec0cc0d3442221285bec801e3e90a17
jhonyrdesouza <jhony.souza@tegrus.com.br>
2024-05-16 17:50:56 -0300

    chore: refactor import statements to use single quotes

--- apps/client/src/lib/i18n.ts
+++ apps/client/src/lib/i18n.ts
@@ -1,6 +1,6 @@
-import { locales } from "@/plugins/i18n/config";
-import { getRequestConfig } from "next-intl/server";
-import { notFound } from "next/navigation";
+import { locales } from '@/plugins/i18n/config';
+import { getRequestConfig } from 'next-intl/server';
+import { notFound } from 'next/navigation';
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
